b9e9f9fde5fdcae23c07e2f968da37acf01f3155
Wesley Willy <wesleywilly@gmail.com>
2017-06-23 17:16:20 -0300

    Quick Guide Update

--- test/SearchCategoryInstance.java
+++ test/SearchCategoryInstance.java
@@ -1,5 +1,7 @@
+import Utilities.FileUtilities;
 import WebCrawlers.Zenodotus;
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 /**
  *
@@ -11,8 +13,18 @@ public class SearchCategoryInstance {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         JSONArray instances = Zenodotus.searchCategoryInstance("matrix");
         System.out.println(instances);
+        
+        /*
+            Saving resulsts in a file.
+        */
+        JSONObject result = new JSONObject();
+        result.put("instances_founded", instances);
+        
+        FileUtilities.save(result, "result.json");
+        
     }
     
 }
